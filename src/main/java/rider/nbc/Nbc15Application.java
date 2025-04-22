d756b9f853cae529031ad881b8210c17dd457485
김정민 <raon37910@gmail.com>
2025-04-22 16:02:57 +0900

    Initial Commit

---
+++ src/main/java/rider/nbc/Nbc15Application.java
@@ -0,0 +1,13 @@
+package rider.nbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbc15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nbc15Application.class, args);
+	}
+
+}
---
+++ src/test/java/rider/nbc/Nbc15ApplicationTests.java
@@ -0,0 +1,13 @@
+package rider.nbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nbc15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
