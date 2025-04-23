422f3694ceb1a46d9b29fdcfa7dda1a4bd1a627f
sukh115 <sukh1151102@gmail.com>
2025-04-23 20:40:28 +0900

    feat : 카카오 로그인 구현

--- src/main/java/rider/nbc/global/auth/AuthUser.java
+++ src/main/java/rider/nbc/global/auth/AuthUser.java
@@ -1,14 +1,37 @@
 package rider.nbc.global.auth;
 
-
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import rider.nbc.domain.user.entity.Role;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
 
 @Getter
 @RequiredArgsConstructor
-public class AuthUser {
+public class AuthUser implements OAuth2User {
+
     private final Long id;
     private final String email;
+    private final String nickname;
     private final Role role;
+
+    @Override
+    public String getName() {
+        return nickname != null ? nickname : "anonymous";
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return Collections.emptyMap();
+    }
 }
