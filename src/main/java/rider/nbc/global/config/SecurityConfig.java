a58645b47d97d047015fe13980847b0f8e6981fa
sukh115 <sukh1151102@gmail.com>
2025-04-23 16:33:14 +0900

    feat : AuthenticationManager bean으로 등록

--- src/main/java/rider/nbc/global/config/SecurityConfig.java
+++ src/main/java/rider/nbc/global/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -83,4 +85,8 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
+        return auth.getAuthenticationManager();
+    }
 }
