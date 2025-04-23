1220f4e635990455fda688a64cd325aa8bc1d840
sukh115 <sukh1151102@gmail.com>
2025-04-23 16:28:28 +0900

    fix : objectMapper 사용해서 간략화 리팩토링

--- src/main/java/rider/nbc/global/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/rider/nbc/global/jwt/JwtAuthenticationEntryPoint.java
@@ -1,17 +1,23 @@
 package rider.nbc.global.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import rider.nbc.global.jwt.jwtException.JwtAuthenticationException;
+import rider.nbc.global.jwt.jwtException.JwtErrorResponse;
 import rider.nbc.global.jwt.jwtException.JwtExceptionCode;
 
 import java.io.IOException;
 
+@RequiredArgsConstructor
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public void commence(HttpServletRequest request,
@@ -25,18 +31,16 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
             errorCode = jwtEx.getCode();
         }
 
+        JwtErrorResponse errorResponse = new JwtErrorResponse(
+                errorCode.isSuccess(),
+                errorCode.getHttpStatus().value(),
+                errorCode.getMessage()
+        );
+
         response.setStatus(errorCode.getHttpStatus().value());
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
 
-        String errorResponse = String.format("""
-                {
-                  "success": %b,
-                  "status": %d,
-                  "message": "%s"
-                }
-                """, errorCode.isSuccess(), errorCode.getHttpStatus().value(), errorCode.getMessage());
-
-        response.getWriter().write(errorResponse);
+        objectMapper.writeValue(response.getWriter(), errorResponse);
     }
 }
