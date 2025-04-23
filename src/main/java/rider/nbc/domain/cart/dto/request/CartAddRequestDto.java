7d96850365bfeea44d73fd7f9de96fd7778ea34e
ksr-ccb <harang4282@gmail.com>
2025-04-23 16:31:51 +0900

    fix(12) : Add Improved feature

--- src/main/java/rider/nbc/domain/cart/dto/request/CartAddRequestDto.java
+++ src/main/java/rider/nbc/domain/cart/dto/request/CartAddRequestDto.java
@@ -1,5 +1,6 @@
 package rider.nbc.domain.cart.dto.request;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -10,10 +11,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public class CartAddRequestDto {
 
-    @NotBlank(message = "메뉴 선택은 필수입니다.")
+    @Min(value = 1)
+    @NotNull( message = "메뉴 선택은 필수입니다.")
     private Long menuId;
 
-    @Size(min = 1, message = "메뉴는 1개이상부터 선택가능합니다.")
+    @Min(value = 1, message = "메뉴는 1개이상부터 선택가능합니다.")
     @NotNull(message = "장바구니에 담으려면 수량을 필수로 입력해야합니다.")
     private int quantity;
     //private List<int> options;
